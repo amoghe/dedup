b4fa6004d06ad30c601bf43d999857e30b54f2f3
Akshay <akshay.moghe@gmail.com>
2017-07-30 11:49:59 -0700

    Move codec message definition to codec.go

--- codec/codec.go
+++ codec/codec.go
@@ -6,3 +6,18 @@ type SegmentWriter interface {
 	Write(seg []byte, id uint64, seen bool) error
 	Close()
 }
+
+const (
+	// GobMessageRef indicates this is a Ref message
+	GobMessageRef = 1
+	// GobMessageDef indicates this is a Def message
+	GobMessageDef = 2
+)
+
+// GobMessage is the message that we write to the output stream.
+type GobMessage struct {
+	Type     uint16
+	RefID    uint64
+	DefID    uint64
+	DefBytes []byte
+}
--- codec/writer_gob.go
+++ codec/writer_gob.go
@@ -60,18 +60,3 @@ func (g *GobWriter) emit(msg *GobMessage) error {
 	}
 	return nil
 }
-
-const (
-	// GobMessageRef indicates this is a Ref message
-	GobMessageRef = 1
-	// GobMessageDef indicates this is a Def message
-	GobMessageDef = 2
-)
-
-// GobMessage is the message that we write to the output stream.
-type GobMessage struct {
-	Type     uint16
-	RefID    uint64
-	DefID    uint64
-	DefBytes []byte
-}
