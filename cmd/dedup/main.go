4f4e54320542b41adc12fac84ac5cff45c51f9ed
Akshay <akshay.moghe@gmail.com>
2017-08-06 19:01:43 -0700

    Fix how source/sink (in/out) streams are set in dedup program

--- cmd/dedup/main.go
+++ cmd/dedup/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/amoghe/dedup"
 	"github.com/pkg/profile"
@@ -33,16 +36,11 @@ var (
 	toStdout = kingpin.Flag("stdout", "Write to stdout").
 			Short('c').
 			Bool()
-	inputFile = kingpin.Arg("infile", "File to be deduplicated").
+	inputFile = kingpin.Arg("infile", "File to be {de|re}duplicated").
 			File()
-
-	source io.ReadCloser  = os.Stdin
-	sink   io.WriteCloser = os.Stdout
 )
 
-// main entrypoint
 func main() {
-
 	kingpin.Parse()
 
 	if *windowSize <= 1 {
@@ -53,6 +51,18 @@ func main() {
 		log.Fatalln("Mask size too small (<=1)")
 	}
 
+	source, err := getInputStream()
+	if err != nil {
+		log.Fatalln("Failed to setup input stream:", err)
+	}
+
+	sink, err := getOutputStream()
+	if err != nil {
+		log.Fatalln("Failed to setup input stream:", err)
+	}
+
+	defer source.Close()
+	defer sink.Close()
 	if *memProfile {
 		defer profile.Start(profile.MemProfile).Stop()
 	}
@@ -64,52 +74,50 @@ func main() {
 	}
 }
 
-// set source/sink (input/output) streams
-// if input is file, output can be file or stdout
-// if input is stdin, output can only be stdout
-func setInputOutpuStreams() {
-	// trivial case
+func getInputStream() (io.ReadCloser, error) {
 	if *inputFile == nil {
-		source = os.Stdin
-		sink = os.Stdout
-		return
+		return os.Stdin, nil
 	}
-
-	source = *inputFile
-
-	// change what sink points to
-	if *toStdout == false {
-		inFileName := (*inputFile).Name()
-		outFileName := inFileName + ".dd"
-		// TODO: if decompression, strip the 'dd'
-
-		inStat, err := (*inputFile).Stat()
-		if err != nil {
-			log.Fatalln("Failed to stat input file:", err)
-		}
-		out, err := os.OpenFile(outFileName, os.O_CREATE|os.O_RDWR, inStat.Mode())
-		if err != nil {
-			log.Fatalln("Failed to open output file:", err)
-		}
-		sink = out
-	}
+	return *inputFile, nil
 }
 
-// Performs deduplication (compression)
-func doDeduplication(in io.ReadCloser, out io.WriteCloser) {
+func getOutputStream() (io.WriteCloser, error) {
+	if *inputFile == nil || *toStdout == true {
+		return os.Stdout, nil
+	}
+
+	var (
+		inFileName   = (*inputFile).Name()
+		dedupFileExt = ".dd"
+		outFileName  = inFileName + dedupFileExt
+	)
+	if *reduplicate == true {
+		if filepath.Ext(inFileName) != dedupFileExt {
+			return nil, errors.New("Input has unknown ext, can't infer output name")
+		}
+		outFileName = strings.TrimSuffix(inFileName, dedupFileExt)
+	}
+
+	inStat, err := (*inputFile).Stat()
+	if err != nil {
+		return nil, err
+	}
+	return os.OpenFile(outFileName, os.O_CREATE|os.O_RDWR, inStat.Mode())
+}
+
+func doDeduplication(in io.Reader, out io.Writer) {
 	dedup := dedup.NewDeduplicator(*windowSize, uint64((1<<*zeroBits)-1))
 	if err := dedup.Do(in, out); err != nil {
-		log.Fatalln("Failed to parse file:", err)
+		log.Fatalln("Failed to deduplicate:", err)
 	}
 	// Print stats (TODO: make this optional)
 	dedup.PrintStats(os.Stderr)
 }
 
-// Performs reduplication (decompression)
-func doReduplication(in io.ReadCloser, out io.WriteCloser) {
+func doReduplication(in io.Reader, out io.Writer) {
 	redup := dedup.NewReduplicator()
 	if err := redup.Do(in, out); err != nil {
-		log.Fatalln("Failed to redup", err)
+		log.Fatalln("Failed to reduplicate:", err)
 	}
 	// Print stats (TODO: make this optional)
 	// redup.PrintStats()
